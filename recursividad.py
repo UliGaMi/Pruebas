bafa0151daeea967da008cab21fdeca81f4123cb
UliGaMi <ulises.galvez@hotmail.com>
2023-03-31 19:44:36 -0600

    Se agregaron entradas y salidas

--- recursividad.py
+++ recursividad.py
@@ -1,5 +1,6 @@
+
+#imprime el valor que se encuentra en la posición n en la serie de fibonacci
 def fibonacci(n):
-    lista = [];
     if n == 2:
         return 1
     elif n == 1:
@@ -8,14 +9,14 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 #imprime la serie en forma de arreglo
-def serieFibonnacci(n):
+def serieFibonacci(n):
     lista = [0,1];
     if n >= 2:   
         while(len(lista) < n):
-            lista.append(lista[len(lista)-1] + lista[len(lista) - 2])
+            lista.append(lista[-1] + lista[-2])
     else:
         return [0]
     return lista
-
-        
+num = input("Ingresa el número de la posición hasta la cual quieres ver la serie de fibonacci: ")
+print(f"El número que está en la posición {num} de la serie de fibonacci es: \n{fibonacci(int(num))}\nLa serie de fibonacci hasta la posición {num} es: \n{serieFibonacci(int(num))}") 
     
